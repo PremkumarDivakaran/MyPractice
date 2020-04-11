70ab6f31240ee447227512f14e5f561c5cfca9f0
Premkumar Divakaran <premkumar.divakaran@oracle.com>
2020-04-11 11:51:22 +0530

    updating gitignore

--- src/test/java/javaProgram/MaxOccuranceInFile.java
+++ src/test/java/javaProgram/MaxOccuranceInFile.java
@@ -1,7 +1,22 @@
 package javaProgram;
 
+import org.testng.annotations.Test;
+
 public class MaxOccuranceInFile {
 
-    
+    @Test
+    public void learning(){
+
+        String text = "geeg";
+
+        StringBuilder stBuild = new StringBuilder(text);
+
+        stBuild.reverse();
+
+        System.out.println(stBuild.toString());
+
+    }
+
+
 
 }
